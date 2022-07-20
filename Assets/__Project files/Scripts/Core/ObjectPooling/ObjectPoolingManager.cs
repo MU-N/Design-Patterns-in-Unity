404fce47846c4f6379a144d5d4355f3647c1190c
MO Nasser <39590845+MU-N@users.noreply.github.com>
2022-07-20 15:57:26 +0200

    adding dotween

---
+++ Assets/__Project files/Scripts/Core/ObjectPooling/ObjectPoolingManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Nasser.io.DesignPatterns
+{
+    public class ObjectPoolingManager : MonoBehaviour
+    {
+        [SerializeField] GameObject objectToSpwan;
+        [SerializeField] int startAmount;
+       // private ObjectPool<SpikeController>();
+
+       
+    }
+}
---
+++ Assets/__Project files/Scripts/Core/ObjectPooling/SpikeController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nasser.io.DesignPatterns
+{
+    public class SpikeController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
