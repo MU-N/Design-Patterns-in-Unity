51462f733451ac517dc26e7ac3f6ec70871f94b4
MO Nasser <39590845+MU-N@users.noreply.github.com>
2022-07-20 16:53:06 +0200

    adding object pooling

--- Assets/__Project files/Scripts/Core/ObjectPooling/ObjectPoolingManager.cs
+++ Assets/__Project files/Scripts/Core/ObjectPooling/ObjectPoolingManager.cs
@@ -7,10 +7,41 @@ namespace Nasser.io.DesignPatterns
 {
     public class ObjectPoolingManager : MonoBehaviour
     {
-        [SerializeField] GameObject objectToSpwan;
+        [SerializeField] SpikeController spikeToSpwan;
         [SerializeField] int startAmount;
-       // private ObjectPool<SpikeController>();
+        private ObjectPool<SpikeController> spikePool;
 
-       
+
+        private void Start()
+        {
+            spikePool = new ObjectPool<SpikeController>(() =>
+            {
+                return Instantiate(spikeToSpwan);
+            }, spike =>
+            {
+                spike.gameObject.SetActive(true);
+            }, spike =>
+            {
+                spike.gameObject.SetActive(false);
+            }, spike =>
+            {
+                Destroy(spike.gameObject);
+            }, false, startAmount, 100);
+        }
+
+
+        public void SpawnNewSpike()
+        {
+            var tempSpike = spikePool.Get();
+            tempSpike.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Random.value, Random.value,10));
+            StartCoroutine(nameof(Destroy), tempSpike);
+        }
+
+        IEnumerator Destroy(SpikeController _spike)
+        {
+
+            yield return new WaitForSeconds(5f);
+            spikePool.Release(_spike);
+        }
     }
 }
